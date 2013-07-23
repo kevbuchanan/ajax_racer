6b6d1cf1818f02fab9ae6223364c2c79e492b2ff
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-23 09:46:06 -0500

    reworking the ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,27 +4,15 @@ get '/' do
 end
 
 post '/new_game' do
-  @player1 = Player.find_or_create_by_name(params[:player1][:name])
-  @player2 = Player.find_or_create_by_name(params[:player2][:name])
-  if @player1.valid? && @player2.valid?
-    @game = Game.new(players: [@player1, @player2])
-    if @game.save
-      erb :_game, layout: false, locals: {player1: @player1, player2: @player2}
-    else
-      @error = @game.players_games.map(&:errors)[1].full_messages.first
-      erb :_form, layout: false, locals: {game_error: @error }
-    end
-  else
-    @errors1 = @player1.errors.full_messages
-    @errors2 = @player2.errors.full_messages
-    erb :_form, layout: false, locals: {errors1: @errors1, errors2: @errors2}
-  end
+
+end
+
+post '/new_player' do
+  @player = Player.find_or_create_by_name(params[:player])
+  content_type :json
+  @player.to_json
 end
 
 post '/finished' do
-  @game = Game.last
-  @winner = Player.find_by_name(params[:winner])
-  @game.winner_id = @winner.id
-  @game.save
-  erb :_winner, layout: false, locals: {game: @game, winner: @winner}
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 function finished(){
-  return $('tr td:nth-child(38)').hasClass('active');
+  return $('tr td:last-child').hasClass('active');
 };
 
 
@@ -11,40 +11,42 @@ function update_player_position(player) {
   };
 
 function play(){
-  while (finished() == false) {
-    $(document).keyup(function(key){
-      if (key.keyCode == 65) {
-        update_player_position('player1');
-      }
-      else if (key.keyCode == 76) {
-        update_player_position('player2');
-      }
-      else if (key.keyCode == 67) {
-        update_player_position('player3');
-      }
-    });
-  }
-  var winner = $('tr td:nth-child(38).active').parent().data('name');
+  $(document).keyup(function(key){
+    if (key.keyCode == 65) {
+      update_player_position('player1');
+    }
+    else if (key.keyCode == 76) {
+      update_player_position('player2');
+    }
+  });
+  var winner = $('tr td:last-child.active').parent().data('name');
   $.post('/finished', {winner: winner}, function(response){
-    $('.container').append(response);
+    alert('winner');
   });
 };
 
 
 $(document).ready(function() {
+  $('#start').hide();
+  var players = 0
   $('#player-setup').on('submit', 'form', function(event){
     event.preventDefault();
+    event.stopPropagation();
+    var button = $(this).find('.btn')
     var url = $(this).attr('action');
     var data = $(this).serialize();
-    $.post(url, data, function(response) {
-      if ($(response).hasClass('new-players')) {
-        $('#player-setup').find('form').replaceWith(response);
-      }
-      else {
-        $('#player-setup').hide();
-        $('.game').replaceWith(response);
-        play();
-      }
+    $.post(url, data, function(response){
+      var name = response.player.name;
+      button.replaceWith("<p class='ready'>"+ name +" is ready to race!</p>");
+      players ++;
+      if (players == 2) {
+        $('#start').show();
+      };
     });
+  });
+  $('#start').on('submit', function(event){
+    event.preventDefault;
+    event.stopPropagation;
+    play();
   });
 });
